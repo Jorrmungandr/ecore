204084c428d43b2b29057a7763b2dd026c4637c1
Edgar Marques <edgar@loomi.com.br>
2023-06-04 22:03:59 -0300

    feat: add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+if not os.path.exists('./db'):
+    os.mkdir('./db')
+
+# Setup tables
+if os.path.exists('./db/users.csv'):
+    print('Skipping users table (already set)')
+else:
+    with open('./db/users.csv', 'w', encoding='utf8') as users_table:
+        columns = ['id', 'name', 'role', 'email', 'password']
+
+        users_table.write(f'{",".join(columns)}\n')
+        print('Setup users table\n')
+
+        admin_user = ['1', 'Admin', 'admin', 'admin@cesar.school', 'Password!123']
+        users_table.write(f'{",".join(admin_user)}\n')
+
+        print('Registered admin user:')
+        print(f'- Email: {admin_user[3]}')
+        print(f'- Password: {admin_user[4]}')
+
+        users_table.close()
